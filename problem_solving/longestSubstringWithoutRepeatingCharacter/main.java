89b81d8a5b125c8cd21b665f54eb35b30ef50135
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-09-25 10:27:35 +0200

    longest substring without repeating character

---
+++ problem_solving/longestSubstringWithoutRepeatingCharacter/main.java
@@ -0,0 +1,9 @@
+package problem_solving.longestSubstringWithoutRepeatingCharacter;
+
+class Main {
+    public static void main(String[] args) {
+        Solution mySolution = new Solution();
+        mySolution.lengthOfLongestSubstring("abcabcabcbb");
+
+    }
+}
