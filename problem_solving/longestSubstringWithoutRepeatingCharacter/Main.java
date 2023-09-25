504e5fe2c25edaf672c9ad118d8580e6064acf59
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-09-25 17:52:11 +0200

    Java is awesome

--- problem_solving/longestSubstringWithoutRepeatingCharacter/Main.java
+++ problem_solving/longestSubstringWithoutRepeatingCharacter/Main.java
@@ -1,7 +1,6 @@
 class Main {
-
     public static void main(String[] args) {
         Solution mySolution = new Solution();
-        System.out.println(mySolution.lengthOfLongestSubstring("abcabcabcbb"));
+        System.out.println(mySolution.lengthOfLongestSubstring("ckilbkd"));
     }
 }
